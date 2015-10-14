f4d0d9777e6d695e948618197272cd7b8a9741e0
Munir Wanis <munirwanis@users.noreply.github.com>
2015-10-14 14:21:43 -0300

    Update Ex1.java

--- L1/E1/src/Ex1.java
+++ L1/E1/src/Ex1.java
@@ -1,25 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author munir
+ * @author Munir Wanis
  */
 public class Ex1 {
 
     /**
-     * @param args the command line arguments
+     * Programa principal
+     * @param args 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        String firstString = "ATCGAT";
-        String secondString = "GATTCTAGA";
-        
-        System.out.println(firstString.compareTo(secondString));
-        
+    public static void main(String[] args) {          
+       
+        try
+        {
+            String result = Ex1.crops(args);
+            System.out.printf("%s", result);
+        }
+        catch(IllegalArgumentException ex)
+        {
+            System.out.printf("IllegalArgumentException: %s", ex.getMessage());
+        }
+        catch(Exception ex)
+        {
+            System.out.printf("%s: %s", ex.getClass(), ex.getMessage());
+        }
     }
     
+    /**
+     * 
+     * @param args 
+     * @return 
+     * @throws IllegalArgumentException 
+     */
+    public static String crops(String[] args) throws IllegalArgumentException
+    {       
+        if(args.length < 2)
+        {
+            throw new IllegalArgumentException("Arguments number must be bigger then 2");
+        }
+        
+        String result = args[0];
+        
+        for(int i=1 ; i<args.length ; i++)
+        {
+            String arg = args[i];
+            String partToReplace = "";
+                        
+            for(int j=1 ; j<arg.length() ; j++)
+            {
+                String part = arg.substring(0, j);
+                
+                if(result.contains(part))
+                {
+                    partToReplace = part;
+                }
+            }            
+            
+            result +=  arg.replaceFirst(partToReplace, "");
+        }
+        
+        return result;
+    }
 }
