faedfc393992d9c81cbabb27fa2577f7276748cb
Munir Wanis <fallen.angel.pp@gmail.com>
2015-10-25 22:17:04 -0200

    Adição de funções no menu e correções na classe de ordenação

--- L1/E3/src/Ex3.java
+++ L1/E3/src/Ex3.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,6 +19,40 @@ public class Ex3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            MinhaListaOrdenavel minhaListaOrdenavel = new MinhaListaOrdenavel();
+           
+            menu(minhaListaOrdenavel);
+
+        } catch (Exception e) {
+            System.out.printf("%s: %s\n", e.getCause(), e.getMessage());
+        }
     }
-    
+
+    private static void menu(MinhaListaOrdenavel minhaListaOrdenavel) {
+        System.out.println(
+                "1.Imprimir Lista\n"
+              + "2.Sair\n"
+        );
+        
+        BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(System.in));        
+        try {
+            
+            int inputValue = bufferedReader.read() - 48;
+            
+            switch (inputValue) {
+                case 1:
+                    inputValue = bufferedReader.read() - 48;
+                    minhaListaOrdenavel.menu();
+                case 2:
+                    break;
+                default:
+                    System.out.println("Opcao nao encontradan.\n");
+                    menu(minhaListaOrdenavel);
+            }
+        } catch (IOException e) {
+            System.out.printf("%s: %s\n", e.getCause(), e.getMessage());
+        }
+    }
 }
--- L1/E3/src/MinhaListaOrdenavel.java
+++ L1/E3/src/MinhaListaOrdenavel.java
@@ -1,67 +1,130 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
- * @author munir
+ * @author Munir Wanis
  */
 public class MinhaListaOrdenavel {
-    
+
     ArrayList<Produto> productList = new ArrayList<>();
-    
-    public static Comparator<Produto> ascendingNameComparator = new Comparator<Produto>() {
+
+    private static Comparator<Produto> ascendingNameComparator = new Comparator<Produto>() {
 
         @Override
-	public int compare(Produto s1, Produto s2) {
-	   String StudentName1 = s1.getNome().toUpperCase();
-	   String StudentName2 = s2.getNome().toUpperCase();
+        public int compare(Produto s1, Produto s2) {
+            String StudentName1 = s1.getNome().toUpperCase();
+            String StudentName2 = s2.getNome().toUpperCase();
 
-	   //ascending order
-	   return StudentName1.compareTo(StudentName2);
-    }};
-    
-    public static Comparator<Produto> descendingNameComparator = new Comparator<Produto>() {
+            //ascending order
+            return StudentName1.compareTo(StudentName2);
+        }
+    };
+
+    private static Comparator<Produto> descendingNameComparator = new Comparator<Produto>() {
 
         @Override
-	public int compare(Produto s1, Produto s2) {
-	   String StudentName1 = s1.getNome().toUpperCase();
-	   String StudentName2 = s2.getNome().toUpperCase();
+        public int compare(Produto s1, Produto s2) {
+            String StudentName1 = s1.getNome().toUpperCase();
+            String StudentName2 = s2.getNome().toUpperCase();
 
-	   //descending order
-	   return StudentName2.compareTo(StudentName1);
-    }};
-    
-    public void menu(int value) {
+            //descending order
+            return StudentName2.compareTo(StudentName1);
+        }
+    };
+
+    private static Comparator<Produto> minorPriceComparator = new Comparator<Produto>() {
+
+        @Override
+        public int compare(Produto product1, Produto product2) {
+            double ProductPrice1 = product1.getPreco();
+            double ProductPrice2 = product2.getPreco();
+
+            //ascending order
+            return (int) Math.round(ProductPrice1 - ProductPrice2);
+        }
+    };
+
+    private static Comparator<Produto> majorPriceComparator = new Comparator<Produto>() {
+
+        @Override
+        public int compare(Produto product1, Produto product2) {
+            double ProductPrice1 = product1.getPreco();
+            double ProductPrice2 = product2.getPreco();
+
+            //descending order
+            return (int) Math.round(ProductPrice2 - ProductPrice1);
+        }
+    };
+
+    private static Comparator<Produto> bestSellingComparator = new Comparator<Produto>() {
+
+        @Override
+        public int compare(Produto product1, Produto product2) {
+            long SoldProduct1 = product1.getVendidos();
+            long SoldProduct2 = product2.getVendidos();
+
+            //descending order
+            return (int) (SoldProduct2 - SoldProduct1);
+        }
+    };
+
+    public void menu() {
         System.out.println(
-                "1.Alfabetica (A-Z) - nome do produto\n"
-              + "2.Alfabetica (Z-A) - nome do produto\n"
-              + "3.Menor preco - crescente\n"  
-              + "4.Maior preco - decrescente\n"
-              + "5.Mais vendidos - decrescente pelo mais vendido\n"
+                  "Escolha seu modo de ordenacao\n"
+                + "1.Alfabetica (A-Z) - nome do produto\n"
+                + "2.Alfabetica (Z-A) - nome do produto\n"
+                + "3.Menor preco - crescente\n"
+                + "4.Maior preco - decrescente\n"
+                + "5.Mais vendidos - decrescente pelo mais vendido\n"
         );
-        
-        switch(value) {
-            case 1:
-                Collections.sort(productList, ascendingNameComparator);
-                printProducts();
-            case 2:
-                Collections.sort(productList, descendingNameComparator);
-                printProducts();
+
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        try {
+            int inputValue = bufferedReader.read() - 48;
+
+            switch (inputValue) {
+                case 1:
+                    Collections.sort(productList, ascendingNameComparator);
+                    printProducts();
+                case 2:
+                    Collections.sort(productList, descendingNameComparator);
+                    printProducts();
+                case 3:
+                    Collections.sort(productList, minorPriceComparator);
+                    printProducts();
+                case 4:
+                    Collections.sort(productList, majorPriceComparator);
+                    printProducts();
+                case 5:
+                    Collections.sort(productList, bestSellingComparator);
+                    printProducts();
+                default:
+                    System.out.println("Opcao nao encontrada.");
+                    menu();
+            }
+        } catch (IOException e) {
+            System.out.printf("%s: %s\n", e.getCause(), e.getMessage());
         }
     }
-    
-    public void printProducts() {
-        for (Produto item: productList) {
+
+    /**
+     * Prints the list
+     */
+    private void printProducts() {
+        for (Produto item : productList) {
             System.out.println(item.toString());
         }
     }
