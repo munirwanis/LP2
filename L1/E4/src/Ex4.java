b55a1416bcef907a1f9d5eb597f3f84456bbd418
Munir Wanis <fallen.angel.pp@gmail.com>
2015-10-27 22:45:44 -0200

    Tratamento de exceção

--- L1/E4/src/Ex4.java
+++ L1/E4/src/Ex4.java
@@ -74,61 +74,65 @@ public class Ex4 {
 
         do {
             System.out.print("\nQual o indice do numero voce quer testar: ");
-            Scanner scanner = new Scanner(System.in);
-            int index = scanner.nextInt();
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int index = scanner.nextInt();
 
-            if (this.nums.size() <= index) {
-                System.out.println("\nO indice apontado nao existe.");
-            } else {
-                System.out.println("\n1. Soma");
-                System.out.println("2. Subtracao");
-                System.out.println("3. Multiplicacao");
-                System.out.println("4. Divisao");
-                System.out.println("5. Outro numero");
-                System.out.println("6. Sair");
+                if (this.nums.size() <= index) {
+                    System.out.println("\nO indice apontado nao existe.");
+                } else {
+                    System.out.println("\n1. Soma");
+                    System.out.println("2. Subtracao");
+                    System.out.println("3. Multiplicacao");
+                    System.out.println("4. Divisao");
+                    System.out.println("5. Outro numero");
+                    System.out.println("6. Sair");
 
-                int option = scanner.nextInt();
+                    int option = scanner.nextInt();
 
-                try {
-                    int value;
-                    int result;
-                    switch (option) {
-                        case 1:
-                            System.out.print("\nSomar com qual numero? ");
-                            value = scanner.nextInt();
-                            result = nums.get(index).soma(value);
-                            System.out.println("Resultado: " + String.valueOf(result));
-                            break;
-                        case 2:
-                            System.out.print("\nSubtrair qual numero? ");
-                            value = scanner.nextInt();
-                            result = nums.get(index).sub(value);
-                            System.out.println("Resultado: " + String.valueOf(result));
-                            break;
-                        case 3:
-                            System.out.print("\nMultiplicar por qual numero? ");
-                            value = scanner.nextInt();
-                            result = nums.get(index).mult(value);
-                            System.out.println("Resultado: " + String.valueOf(result));
-                            break;
-                        case 4:
-                            System.out.print("\nDividir por qual numero? ");
-                            value = scanner.nextInt();
-                            result = nums.get(index).div(value);
-                            System.out.println("Resultado: " + String.valueOf(result));
-                            break;
-                        case 5:
-                            break;
-                        case 6:
-                            _continue = false;
-                            break;
-                        default:
-                            System.out.println("\nOpção inválida.");
-                            break;
+                    try {
+                        int value;
+                        int result;
+                        switch (option) {
+                            case 1:
+                                System.out.print("\nSomar com qual numero? ");
+                                value = scanner.nextInt();
+                                result = nums.get(index).soma(value);
+                                System.out.println("Resultado: " + String.valueOf(result));
+                                break;
+                            case 2:
+                                System.out.print("\nSubtrair qual numero? ");
+                                value = scanner.nextInt();
+                                result = nums.get(index).sub(value);
+                                System.out.println("Resultado: " + String.valueOf(result));
+                                break;
+                            case 3:
+                                System.out.print("\nMultiplicar por qual numero? ");
+                                value = scanner.nextInt();
+                                result = nums.get(index).mult(value);
+                                System.out.println("Resultado: " + String.valueOf(result));
+                                break;
+                            case 4:
+                                System.out.print("\nDividir por qual numero? ");
+                                value = scanner.nextInt();
+                                result = nums.get(index).div(value);
+                                System.out.println("Resultado: " + String.valueOf(result));
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                _continue = false;
+                                break;
+                            default:
+                                System.out.println("\nOpção inválida.");
+                                break;
+                        }
+                    } catch (NaoPrimoEx ex) {
+                        System.out.println("Resultado não é primo");
                     }
-                } catch (NaoPrimoEx ex) {
-                    System.out.println("Resultado não é primo");
                 }
+            } catch (Exception e) {
+                System.out.println("Digite um numero inteiro de indice");
             }
 
         } while (_continue);
